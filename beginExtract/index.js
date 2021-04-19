3fddf795daff5ac4b98ee8a05cf66f6b7fbd9119
herkom <omar.guadarrama@rocketmail.com>
2021-04-19 13:32:53 -0500

    firebase da automaticamente el id

--- beginExtract/index.js
+++ beginExtract/index.js
@@ -1,4 +1,4 @@
-//const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 const admin = require('firebase-admin');
 
 const serviceAccount = require('./extracto-2eb04-60cbc8a2630f.json');
@@ -9,22 +9,22 @@ admin.initializeApp({
   
 const db = admin.firestore();
   
-
 module.exports = async function (context, myTimer) {
+    context.log('Function triggered')
 
-    /* fetch('https://app.scrapinghub.com/api/run.json?apikey=5fed10557a124004a04939eb55ef0719',
+    fetch('https://app.scrapinghub.com/api/run.json?apikey=5fed10557a124004a04939eb55ef0719',
         {
             body: "project=515196&spider=osfeSintesis",
             headers: { "Content-Type":"application/x-www-form-urlencoded" },
             method:'POST'
         })
     .then((res)=>res.json())
-    .then((data) => console.log(data.jobid))
-    .catch((err)=> console.log(err))  */
+    .then(async(data) => {
+        const firebaseRes = await db.collection('Jobs').add({
+            jobNumber: data.jobid,
+        });
+    })
+    .catch((err)=> console.log(err));
 
-    const docRef = db.collection('Jobs').doc('brubru');
-
-    await docRef.set({
-        jobNumber: "Prueba"
-    });
+    context.done();
 };
